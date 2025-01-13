8c4e3e247e3d7875d9ed1521cc5619dd40832138
kinbasari <kinbasari@gmail.com>
2025-01-13 16:55:28 +0700

    myscript.js

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,15 +1,69 @@
-// Toggle class active
+// Toggle class active hamburger menu
 const navbarNav = document.querySelector('.navbar-nav');
-// Saat Menu di klik
+// Saat hamburger menu di klik
 document.querySelector('#hamburger-menu').onclick = () => {
   navbarNav.classList.toggle('active');
 };
 
+// Toggle class active search form
+
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Toggle class active shopping cart
+const shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+  shoppingCart.classList.toggle('active');
+  e.preventDefault();
+};
+
+
 // klik luar sidbar hilang nav
 
-const hamburger = document.querySelector('#hamburger-menu');
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
+const sc = document.querySelector('#shopping-cart-button')
+
 document.addEventListener('click', function(e) {
-  if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if(!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove('active');
   }
-})
+  
+  if(!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('active');
+  }
+  
+  if(!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove('active');
+  }
+});
+
+// Modal Box
+const itemDetailModal = document.querySelector('#item-detail-modal-python');
+const itemDetailButton = document.querySelector('.item-detail-button');
+
+itemDetailButton.onclick = (e) => {
+  itemDetailModal.style.display = 'flex';
+  e.preventDefault();
+};
+
+// klik close modal
+document.querySelector('.modal .close-icon').onclick = (e) => {
+  itemDetailModal.style.display = 'none';
+  e.preventDefault();
+};
+
+// klik diluar modal
+const modal = document.querySelector('#item-detail-modal-python');
+window.onclick = (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+}
